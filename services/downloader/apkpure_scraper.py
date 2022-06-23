028406347dcfbc34297b3747e9af8f4f95e371b0
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-23 23:27:44 +0530

    done

--- services/downloader/apkpure_scraper.py
+++ services/downloader/apkpure_scraper.py
@@ -125,7 +125,7 @@ class ApkpureScraper:
         soup = None
         for i in range(0,self.max_retry):
             
-            response = self.wd.get(url,proxies=self.proxy,headers=self.headers)
+            response = self.wd.get(url)
             print(response.status_code)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text)
@@ -140,7 +140,7 @@ class ApkpureScraper:
         file_bytes = None
         
         for i in range(0,self.max_retry):
-            response = self.wd.get(download_link,proxies=self.proxy)
+            response = self.wd.get(download_link)
             if response.status_code == 200:
                 file_bytes = response.content
                 break
