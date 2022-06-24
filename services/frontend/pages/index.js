a43ffc88e92d3faed900b6167c7753af1b92da2f
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-25 01:23:09 +0530

    done

--- services/frontend/pages/index.js
+++ services/frontend/pages/index.js
@@ -1,18 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Search from '../components/search';
-import MyApps from '../components/recent_apps';
-import { backend_base_url, backend__internal_base_url } from '../src/config';
-import { get_auth_token } from '../src/helper';
 import { useRouter } from 'next/router';
 
 export default function IndexRoute() {
   
   const router = useRouter()
-
-  router?.push("/home")
   
+  useEffect(() => {
+        
+    router.push("/home")
 
+  },[])
   
 
   return (
