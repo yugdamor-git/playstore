b0003b19f9620a2bcb787f8387ad1c6b1d94a1bc
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-25 01:24:56 +0530

    done

--- services/frontend/pages/index.js
+++ services/frontend/pages/index.js
@@ -1,6 +1,7 @@
-import * as React from 'react';
+
 import Box from '@mui/material/Box';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function IndexRoute() {
   
