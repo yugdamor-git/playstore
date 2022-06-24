655db1b86e454f299bebfd7107b0f13de99babb2
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-25 00:55:29 +0530

    done

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
     reactStrictMode: true,
-    i18n:{
-      locales:['en','es','fr','ru','id','sv','hr','fi','it','nl','pt','pl','hi','ja','ko','ms','vi','ml','ta','zh','mr','ro','ur','cs','da','de','bn','ar'],
-      defaultLocale:'en'
-    }
+    generateEtags: false
   };
