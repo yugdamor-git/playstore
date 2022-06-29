312b25b01bd71a16640d7dbb6796e088d6c6faaf
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-29 16:01:52 +0530

    done

--- services/frontend/src/config.js
+++ services/frontend/src/config.js
@@ -1,2 +1,2 @@
-export const backend_base_url = "http://148.251.41.232:6001"
+export const backend_base_url = "https://cdn.onehost.io/api"
 export const backend__internal_base_url = "http://backend:5000"
